f862e06c70840cfe2b49530bbdb3718838232297
mushroomvoodoo <normeeboy@gmail.com>
2023-06-22 11:14:01 +0300

    конец наконец-то

--- Converter/Form1.cs
+++ Converter/Form1.cs
@@ -11,6 +11,7 @@ namespace Converter
         {
             InitializeComponent();
             comBoxChange(defaultpose);
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
         }
         
         Dictionary<double, double> wSizes = new Dictionary<double, double>
@@ -137,7 +138,7 @@ namespace Converter
 
         internal void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
